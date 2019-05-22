614607dcb6b36c8bbfe14a676940c7447a32df21
wsbos <462577@edu.rocmn.nl>
2019-05-22 14:52:54 +0200

    Basis layout

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wsbos
+ * Date: 5/22/2019
+ * Time: 02:24 PM
+ */
+?>
+
+<footer>
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+    </footer>
+</footer>
