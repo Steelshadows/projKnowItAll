a13a736e7a7653d0ab39c9a5ad58a5ab1fbc3f74
Brand van Harn <its.a.me.brand@gmail.com>
2019-05-23 10:40:18 +0200

    login/signup system ready for implementation

--- login/login.php
+++ login/login.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 $servername = "127.0.0.1";
-$username = "root";
-$password = "";
+$usernamesqllogin = "root";
+$passwordsqllogin = "";
 $dbname = 'knowitall';
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $usernamesqllogin, $passwordsqllogin, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
@@ -14,31 +14,63 @@ if(isset($_POST['submitsignup'])){
     $passwordCheck = base64_encode(htmlspecialchars($_POST['passwordCheck']));
     $username =     htmlspecialchars($_POST['username']);
     $email = htmlspecialchars($_POST['email']);
-//    $emaildupecheck = $conn->query();
-    $emaildupechecksql = 'SELECT `email` FROM `knowitall_gebruikers` WHERE `email` = ?';
-    $emaildupecheck = $conn->prepare($emaildupechecksql);
-    $emaildupecheck->bind_param('s',$email);
-    $emaildupecheck->execute();
 
     if($password == $passwordCheck) {
-//        $sql = "
-//        INSERT INTO `knowitall_gebruikers` (`gebruikersnaam`, `password`, `email`)
-//        VALUES (?,?,?)
-//        ";
         $sql = '
         INSERT INTO `knowitall_gebruikers` (`gebruikersnaam`, `email`, `wachtwoord`) VALUES (?, ?, ?)
         ';
-        echo $sql;
-        $res = mysqli_query($conn, $sql);
+//        $res = mysqli_query($conn, $sql);
         $statement = $conn->prepare($sql);
         $statement->bind_param('sss',$username, $email,$password);
-        $statement->execute();
-//    if (!$res) {
-//        echo "Failed to run query: (" . $conn->errno . ") " . $conn->error;
-//    }
+        if (!$statement->execute()){
+            if ($conn->errno == 1062){
+                echo 'deze email bestaat al, kies een andere';
+            }
+            else{
+                echo "Failed to add user error: (" . $conn->errno . ") " . $conn->error;
+            }
+        }
+        $UIDcheckSQL='SELECT `gebruiker_ID` FROM `knowitall_gebruikers` WHERE `email` = \''.$conn->real_escape_string($email).'\'';
+//        echo $UIDcheckSQL;
+        $result = $conn->query($UIDcheckSQL);
+        $id = false;
+        while($row = $result->fetch_assoc()) {
+            $UID = (int) $row['gebruiker_ID'];
+        }
+
+    }
+    $_SESSION['user_ID'] = $UID;
+}
+if(isset($_POST['submitlogin'])){
+    $password = base64_encode(htmlspecialchars($_POST['password']));
+//    $username =     htmlspecialchars($_POST['username']);
+    $email = htmlspecialchars($_POST['email']);
+    $loginCheckSQL='SELECT `email`, `wachtwoord`, `gebruiker_ID` FROM `knowitall_gebruikers`';
+//        echo $UIDcheckSQL;
+    $result = $conn->query($loginCheckSQL);
+    while($row = $result->fetch_assoc()) {
+        $check['EM'] = $row['email'];
+        $check['PW'] = $row['wachtwoord'];
+//        var_dump($row);
+        if ($check['EM'] == $email&&$check['PW'] == $password){
+            echo 'login successfull';
+            $_SESSION['user_ID'] = $row['gebruiker_ID'];
+        }
     }
 }
+if(isset($_SESSION['user_ID'])){
+//    echo $_SESSION['user_ID'];
 
+    $usernameSQL='SELECT `gebruikersnaam` FROM `knowitall_gebruikers` WHERE `gebruiker_ID` = \''.$conn->real_escape_string($_SESSION['user_ID']).'\'';
+//    echo $usernameSQL;
+    $result = $conn->query($usernameSQL);
+    while($row = $result->fetch_assoc()) {
+        $username = $row['gebruikersnaam'];
+
+//        var_dump($row);
+    }
+    echo '<br>welkom, '.$username;
+}
 
 
 ?>
@@ -52,19 +84,34 @@ if(isset($_POST['submitsignup'])){
     <title>Document</title>
 </head>
 <body>
-<form method="post">
-    <input type="text" name="email" placeholder="E-Mail" required>
-    <input type="text" name="username" placeholder="gebruikers naam" required>
-    <input type="password" name="password" id="password" placeholder="Wachtwoord" required>
-    <input type="password" name="passwordCheck" placeholder="Herhaling wachtwoord" id="passwordCheck" required>
-    <input type="submit" name="submitsignup" id="submitsignup" style="display: none;">
-</form>
-<button onclick="
-console.log(document.getElementById('password').value);
-console.log(document.getElementById('passwordCheck').value);
-if(document.getElementById('password').value == document.getElementById('passwordCheck').value){
-    document.getElementById('submitsignup').click()
-}
-else{alert('passwords are not the same')}">submit</button>
+<button onclick="document.getElementById('signup').style = 'display:block;';this.style = 'display:none;'">signup</button>
+<div id='signup' style="display: none">
+    <form method="post">
+        <input type="text" name="email" placeholder="E-Mail" required>
+        <input type="text" name="username" placeholder="gebruikers naam" required>
+        <input type="password" name="password" id="password" placeholder="Wachtwoord" required>
+        <input type="password" name="passwordCheck" placeholder="Herhaling wachtwoord" id="passwordCheck" required>
+        <input type="submit" name="submitsignup" id="submitsignup" style="display: none;">
+    </form>
+    <button onclick="
+    console.log(document.getElementById('password').value);
+    console.log(document.getElementById('passwordCheck').value);
+    if(document.getElementById('password').value == document.getElementById('passwordCheck').value){
+        document.getElementById('submitsignup').click()
+    }
+    else{alert('passwords are not the same')}">submit</button>
+</div>
+
+
+
+<button onclick="document.getElementById('login').style = 'display:block;';this.style = 'display:none;'">login</button>
+<div id='login' style="display: none">
+    <form method="post">
+        <input type="text" name="email" placeholder="E-Mail" required>
+        <input type="password" name="password" id="password" placeholder="Wachtwoord" required>
+        <br>
+        <input type="submit" name="submitlogin" id="submitlogin">
+    </form>
+</div>
 </body>
 </html>
