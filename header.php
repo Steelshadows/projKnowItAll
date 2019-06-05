0243c728ef1cb2c87c01284eebafd59e33e12c6c
Roy de Graaf <roydegraaf8@hotmail.com>
2019-06-05 13:23:19 +0200

    Login pagina beetje gestyled

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">My Account</a>
                     <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="login.php">Login</a>
                         <a class="dropdown-item" href="#">Another action</a>
                         <a class="dropdown-item" href="#">Something else here</a>
                         <div class="dropdown-divider"></div>
