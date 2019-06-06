5106ffc2d217242d9042066d9157beae6814a7b8
Roy de Graaf <roydegraaf8@hotmail.com>
2019-06-06 09:34:07 +0200

    signup link in header gezet

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">My Account</a>
                     <div class="dropdown-menu dropdown-menu-right">
                         <a class="dropdown-item" href="login.php">Login</a>
-                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="signup.php">Sign up</a>
                         <a class="dropdown-item" href="#">Something else here</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Separated link</a>
