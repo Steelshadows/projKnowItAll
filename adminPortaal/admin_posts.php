332ed94d700841fc62391b5f42fda6add4cd1594
Brand van Harn <its.a.me.brand@gmail.com>
2019-06-13 11:23:45 +0200

    admin change status page added

---
+++ adminPortaal/admin_posts.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itsam
+ * Date: 6/12/2019
+ * Time: 2:15 PM
+ */
---
+++ adminPortaal/change.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itsam
+ * Date: 6/12/2019
+ * Time: 2:34 PM
+ */
