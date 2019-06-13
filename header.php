7f228cad5a01bb4a6b3df3c2634ba594ee95f09f
Roy de Graaf <roydegraaf8@hotmail.com>
2019-06-13 09:18:29 +0200

    Nav aangepast zodat je alleen kan inloggen als je niet bent ingelogt

--- header.php
+++ header.php
@@ -5,8 +5,13 @@
  * Date: 5/22/2019
  * Time: 02:24 PM
  */
-$session = null;
+$uitloggen = null;
+$inloggen = '<a class="dropdown-item" href="login.php">Login</a>
+             <a class="dropdown-item" href="signup.php">Registreren</a>';
 if (isset($_SESSION['user_ID'])) {
+    $uitloggen = '<div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="uitloggen.php">Uitloggen</a>';
+    $inloggen = null;
 }
 ?>
 
@@ -27,11 +32,9 @@ if (isset($_SESSION['user_ID'])) {
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">My Account</a>
                     <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="login.php">Login</a>
-                        <a class="dropdown-item" href="signup.php">Sign up</a>
+                        <?=$inloggen?>
                         <a class="dropdown-item" href="#">Something else here</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="uitloggen.php">Uitloggen</a>
+                        <?=$uitloggen?>
                     </div>
                 </li>
             </ul>
